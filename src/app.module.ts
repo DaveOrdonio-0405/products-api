f9e91cbc38c0ed4558136047ca2e6d71d0e5f96f
DaveOrdonio-0405 <daveordonio92@gmail.com>
2024-09-06 22:57:56 +0800

    Add TypeORM configuration for MySQL and CacheModule configuration for Redis.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ProductModule } from './product/product.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'nest_products_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CacheModule.register({
+      store: 'ioredis',
+      host: 'localhost',
+      port: 6379,
+    }),
+    ProductModule,
+  ],
 })
 export class AppModule {}
